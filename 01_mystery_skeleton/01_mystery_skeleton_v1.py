44312fd9f3d1c1df8003b44fd4b242a21e9cf122
vickyhuanggg <vickyhua@pnghs.school.nz>
2022-04-04 11:51:26 +1200

    Update 01_mystery_skeleton_v1.py

--- 01_mystery_skeleton/01_mystery_skeleton_v1.py
+++ 01_mystery_skeleton/01_mystery_skeleton_v1.py
@@ -42,6 +42,38 @@ class Game:
         # Set starting balance to amount entered by user at start of game
         self.balance.set(starting_balance)
 
+        # GUI Setup
+        self.game_box = Toplevel()
+        self.game_frame = Frame(self.game_box)
+        self.game_frame.grid()
+
+        # Heading Row
+        self.heading_label = Label(self.game_frame, text="Heading",
+                                   font="Arial 24 bold", padx=10, pady=10)
+        self.heading_label.grid(row=0)
+
+        # balance
+        self.balance_label = Label(self.game_frame, text="")
+        self.balance_label.grid(row=1)
+
+        self.gain_button = Button(self.game_frame, text="Gain",
+                                  command=partial(self.reveal_boxes))
+        self.gain_button.grid(row=2)
+
+    def reveal_boxes(self):
+        # retrieve the balance from the initial function
+        current_balance = self.balance.get()
+
+        # Adjust the balance (substract game cost and add pay out)
+        # for testing purposes, just add 2
+        current_balance += 2
+
+        # set balance to adjusted balance
+        self.balance.set(current_balance)
+
+        # Edit label so user can see their balance
+        self.balance_label.configure(text="Balance: {}".format(current_balance))
+
 
 # main routine
 if __name__ == "__main__":
