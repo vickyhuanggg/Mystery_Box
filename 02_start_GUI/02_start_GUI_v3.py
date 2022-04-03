fbce5560f2d6d2a55c866588e83b1e3f6a0b6067
vickyhuanggg <vickyhua@pnghs.school.nz>
2022-04-04 11:51:56 +1200

    Update 02_start_GUI_v3.py

--- 02_start_GUI/02_start_GUI_v3.py
+++ 02_start_GUI/02_start_GUI_v3.py
@@ -147,8 +147,32 @@ class Game:
         print(stakes)
         print(starting_balance)
 
+        # disable low stakes button
+        partner.lowstakes_button.config(state=DISABLED)
 
+        # initialise variables
+        self.balance = IntVar()
 
+        # Set starting balance to amount entered by user at start of game
+        self.balance.set(starting_balance)
+
+        # GUI Setup
+        self.game_box = Toplevel()
+        self.game_frame = Frame(self.game_box)
+        self.game_frame.grid()
+
+        # Heading Row
+        self.heading_label = Label(self.game_frame, text="Heading",
+                                   font="Arial 24 bold", padx=10, pady=10)
+        self.heading_label.grid(row=0)
+
+        # balance
+        self.balance_label = Label(self.game_frame, text="")
+        self.balance_label.grid(row=1)
+
+        self.gain_button = Button(self.game_frame, text="Gain",
+                                  command=partial(self.reveal_boxes))
+        self.gain_button.grid(row=2)
 
     def reveal_boxes(self):
         # retrieve the balance from the initial function
