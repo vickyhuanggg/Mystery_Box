9d22c8dbe501991fcd770836846a483670649944
vickyhuanggg <vickyhua@pnghs.school.nz>
2022-05-02 10:41:47 +1200

    Update 03_game_GUI_v1.py

--- 03_game_GUI/03_game_GUI_v1.py
+++ 03_game_GUI/03_game_GUI_v1.py
@@ -29,8 +29,6 @@ class Game:
         print(stakes)
         print(starting_balance)
 
-        # disable low stakes button
-        partner.lowstakes_button.config(state=DISABLED)
 
         # initialise variables
         self.balance = IntVar()
@@ -38,23 +36,72 @@ class Game:
         # Set starting balance to amount entered by user at start of game
         self.balance.set(starting_balance)
 
+        # Get value of stakes (use it as a multiplier when calculating winnings
+        self.multiplier = IntVar()
+        self.multiplier.set(stakes)
+
         # GUI Setup
         self.game_box = Toplevel()
         self.game_frame = Frame(self.game_box)
         self.game_frame.grid()
 
         # Heading Row
-        self.heading_label = Label(self.game_frame, text="Heading",
+        self.heading_label = Label(self.game_frame, text="Play...",
                                    font="Arial 24 bold", padx=10, pady=10)
         self.heading_label.grid(row=0)
 
-        # balance
-        self.balance_label = Label(self.game_frame, text="")
-        self.balance_label.grid(row=1)
+        # Instructions Label
+        self.instructions_label = Label(self.game_frame, wrap=300, justify=LEFT,
+                                        text="Press <enter> or click the 'Open "
+                                        "Boxes' button to reveal the "
+                                        "contents of the mystery boxes.",
+                                        font="Arial 10", padx=10, pady=10)
+        self.instructions_label.grid(row=1)
 
-        self.gain_button = Button(self.game_frame, text="Gain",
-                                  command=partial(self.reveal_boxes))
-        self.gain_button.grid(row=2)
+        # Boxes go here (row 2)
+        box_text = "Arial 16 bold"
+        box_back = "#b9ea96" #light green
+        box_width = 5
+        self.box_frame = Frame(self.game_frame)
+        self.box_frame.grid(row=2, pady=10)
+
+        self.prize1_label = Label(self.box_frame, text="?\n", font=box_text,
+                                  bg=box_back, width=box_width, padx=10, pady=10)
+        self.prize1_label.grid(row=0, column=0)
+
+        self.prize2_label = Label(self.box_frame, text="?\n", font=box_text,
+                                  bg=box_back, width=box_width, padx=10, pady=10)
+        self.prize2_label.grid(row=0, column=1, padx=10)
+
+        self.prize3_label = Label(self.box_frame, text="?\n", font=box_text,
+                                  bg=box_back, width=box_width, padx=10, pady=10)
+        self.prize3_label.grid(row=0, column=2)
+
+        # Play button goes here (row 3)
+        self.play_button = Button(self.game_frame, text="Open Boxes",
+                                  bg="#FFFF33", font="Arial 15 bold", width=20,
+                                  padx=10, pady=10, command=self.reveal_boxes)
+        self.play_button.grid(row=3)
+
+        # Balance Label(row 4)
+
+        start_text = "Welcome, your starting balance is $50"
+
+        self.balance_label = Label(self.game_frame, font="Arial 12 bold",
+                                   fg="green", text=start_text, wrap=300, justify=LEFT)
+        self.balance_label.grid(row=4, pady=10)
+
+        # Help and Game Stats button (row 5)
+        self.help_export_frame = Frame(self.game_frame)
+        self.help_export_frame.grid(row=5, pady=10)
+
+        self.help_button = Button(self.help_export_frame, text="Help / Rules",
+                                  font="Arial 15 bold", bg="#808080", fg="white")
+        self.help_button.grid(row=0, column=0, padx=2)
+
+        self.starts_button = Button(self.help_export_frame, text="Game Stats...",
+                                    font="Arial 15 bold", bg="#003366", fg="white")
+        self.starts_button.grid(row=0, column=1, padx=2)
 
     def reveal_boxes(self):
         # retrieve the balance from the initial function
